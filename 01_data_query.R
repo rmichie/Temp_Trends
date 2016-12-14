5fd5be46f8ed6ea31f7f619b4284ea63f2d9406c
rmichie <michie.ryan@deq.state.or.us>
2016-12-14 12:11:06 -0800

    lasarQuery update
    Updated lasarQuery function to query based on stations rather than HUCs
    due to a number of missing stations in the XLU_AREA table within LASAR2

--- 01_data_query.R
+++ 01_data_query.R
@@ -6,30 +6,35 @@ library(RODBC)
 
 options(stringsAsFactors = FALSE)
 
-fun_dir <- 'F:/WorkSpace/Quantifying_Conservation_2014/SouthernWillamette/TempTrends'
-data_query_dir <- 'F:/WorkSpace/Quantifying_Conservation_2014/SouthernWillamette/TempTrends/data_query'
+fun_dir <- 'C:/WorkSpace/GitHub/Temp_Trends'
+data_query_dir <- 'F:/WorkSpace/MidCoast/Temp_Data/data_query'
 
-# Middle Fork Willamette (17090001)
-# Coast Fork Willamette (17090002)
-# Upper Willamette (17090003)
-# McKenzie (17090004)
-# North Santiam (17090005)
-# South Santiam (17090006)
 
-HUClist <- c(17090001,17090002,17090003,17090004,17090005,17090006)
+# HUC 8
+# 17100204 -- Siletz-Yaquina 
+# 17100205 -- Alsea
+# 17100206 -- Siuslaw
+# 17100207 -- Siltcoos
+
+HUClist <- c(17100204,17100205,17100206,17100207)
 
 input <- list()
 input$parms <- 'Temperature'
-input$dates <- c("1998-06-01", "2015-10-31")
+input$dates <- c("1999-06-01", "2015-10-31")
 input$DQL <- "'A+', 'A', 'B'"
 input$siteTypeLASAR <- "'Surface water'"
 input$siteTypeNWIS <- "ST,ST-TS"
 
+
 setwd(fun_dir)
 source('funDataQuery.R')
 source('funClean.R')
+stations_wbd <- read.csv('station_wbd_12132016.csv',stringsAsFactors = FALSE)
 
 for (i in 1:length(HUClist)) {
+  
+  # Get all the LASAR stations within each HUC8
+  stationlist <- stations_wbd[stations_wbd$HUC8 %in% HUClist[i],]$STATION_KEY
   
   wqpData <- NULL
   nwisData <- NULL
@@ -49,13 +54,15 @@ for (i in 1:length(HUClist)) {
                                  siteTypeCd = input$siteTypeNWIS),
                        error = function(err) {err <- geterrmessage()})
   
-   lasarData <- lasarQuery(HUClist = HUClist[i],
-                           inParms = input$parms,
-                           startDate = input$dates[1],
-                           endDate = input$dates[2],
-                           DQL = input$DQL,
-                           siteType = input$siteTypeLASAR)
+  lasarData <- lasarQuery(stationlist = stationlist,
+                          inParms = input$parms,
+                          startDate = input$dates[1],
+                          endDate = input$dates[2],
+                          DQL = input$DQL,
+                          siteType = input$siteTypeLASAR)
   odbcCloseAll()
+  
+  if (is.data.frame(lasarData)) {lasarData$HUC <- HUClist}
   
   if (!is.data.frame(nwisData)) {nwisData <- NULL}
   df.all <- combine(W=wqpData,L=lasarData,N=nwisData)
